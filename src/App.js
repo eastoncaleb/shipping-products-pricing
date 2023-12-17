4d8a13a46677268c73b3051f6e205ef047e6ba66
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-12-17 14:04:50 -0500

    Edit App.js with basic button and modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import PriceModal from './PriceModal';
 
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const toggleModal = () => setModalIsOpen(!modalIsOpen);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={toggleModal}>Open Modal</button>
+      <PriceModal isOpen={modalIsOpen} toggleModal={toggleModal} />
     </div>
   );
 }
