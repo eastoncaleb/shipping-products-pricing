022413f911755640c3f6fc8f472edadec1e7a02b
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-12-17 22:53:18 -0500

    Style changes to App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ function App() {
   const toggleModal = () => setModalIsOpen(!modalIsOpen);
 
   return (
-    <div className="App">
-      <button onClick={toggleModal}>Open Modal</button>
+    <div className="App p-10 flex justify-center">
+      <button
+        className="text-lg bg-blue-500 rounded-lg border-b-4 border-blue-600 focus:border-t-4 focus:border-b-0 focus:shadow-inner mx-auto p-4 font-bold text-white hover:bg-blue-400"
+        onClick={toggleModal}
+      >
+        Open Price Modal
+      </button>
       <PriceModal isOpen={modalIsOpen} toggleModal={toggleModal} />
     </div>
   );
